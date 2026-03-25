852b2fc616e566567e26c4507df7d57decbe62c2
Ranjot Singh <rsingh18@rrc.ca>
2026-03-24 23:58:41 -0500

    I.1 I.2 I.3 - Implement UserRecipe repository with CRUD, test data integration, service layer, custom hook, and component integration using Hook-Service-Repository architecture

---
+++ src/repositories/UserRecipeRepository.ts
@@ -0,0 +1,34 @@
+import type { UserRecipe } from "../types/UserRecipe";
+import { userRecipesData } from "../data/userRecipeData";
+
+let recipes: UserRecipe[] = [...userRecipesData];
+
+export class UserRecipeRepository {
+  static getAll(): Promise<UserRecipe[]> {
+    return Promise.resolve(recipes.map(r => ({ ...r, ingredients: [...r.ingredients], instructions: [...r.instructions] })));
+  }
+
+  static getById(id: number): Promise<UserRecipe | undefined> {
+    const recipe = recipes.find(r => r.id === id);
+    return Promise.resolve(
+      recipe ? { ...recipe, ingredients: [...recipe.ingredients], instructions: [...recipe.instructions] } : undefined
+    );
+  }
+
+  static create(recipe: UserRecipe): Promise<UserRecipe> {
+    const toStore: UserRecipe = { ...recipe, ingredients: [...recipe.ingredients], instructions: [...recipe.instructions] };
+    recipes.push(toStore);
+    return Promise.resolve({ ...toStore, ingredients: [...toStore.ingredients], instructions: [...toStore.instructions] });
+  }
+
+  static update(updatedRecipe: UserRecipe): Promise<UserRecipe> {
+    const toStore: UserRecipe = { ...updatedRecipe, ingredients: [...updatedRecipe.ingredients], instructions: [...updatedRecipe.instructions] };
+    recipes = recipes.map(r => (r.id === toStore.id ? toStore : r));
+    return Promise.resolve({ ...toStore, ingredients: [...toStore.ingredients], instructions: [...toStore.instructions] });
+  }
+
+  static delete(id: number): Promise<void> {
+    recipes = recipes.filter(r => r.id !== id);
+    return Promise.resolve();
+  }
+}
